9e42c4a4ee285a8cd95823a8f6cbc4ccab756147
DukeAnn <adk@adki.me>
2023-09-18 21:22:23 +0800

    修正单词拼写，更新新的UUID包。（Fix word spelling, update new UUID package）

--- client/extra.go
+++ client/extra.go
@@ -1,26 +1,20 @@
 package client
 
 import (
-	"fmt"
-	"math/rand"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 type ExtraGenerator func() string
 
 func UuidV4Generator() ExtraGenerator {
 	return func() string {
-		var uuid [16]byte
-		rand.Read(uuid[:])
-
-		uuid[6] = (uuid[6] & 0x0f) | 0x40
-		uuid[8] = (uuid[8] & 0x3f) | 0x80
-
-		return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x", uuid[:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
+		return uuid.NewString()
 	}
 }
 
-func IsCommmand(text string) bool {
+func IsCommand(text string) bool {
 	if text != "" {
 		if text[0] == '/' {
 			return true
@@ -30,7 +24,7 @@ func IsCommmand(text string) bool {
 }
 
 func CheckCommand(text string, entities []*TextEntity) string {
-	if IsCommmand(text) {
+	if IsCommand(text) {
 		// Check text entities and make bot happy!
 		if len(entities) >= 1 {
 			// Get first command
@@ -61,7 +55,7 @@ func CheckCommand(text string, entities []*TextEntity) string {
 }
 
 func CommandArgument(text string) string {
-	if IsCommmand(text) {
+	if IsCommand(text) {
 		// e.g. ["/hello 123", "/hell o 123"]
 		// Result: "123", "o 123"
 		if i := strings.Index(text, " "); i != -1 {
